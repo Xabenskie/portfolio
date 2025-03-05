ce87f35314fc44a7ab96deea5e111058fb7ce4ce
Xabenskie <lukyanovkr@gmail.com>
2025-03-05 11:46:44 +0300

    [*] правки семантики и логики js

--- js/main.js
+++ js/main.js
@@ -1,44 +1,39 @@
 const btnDarkMode = document.querySelector('.dark-mode-btn')
 
-if (localStorage.getItem('darkMode') === 'dark') {
-	btnDarkMode.classList.add('dark-mode-btn--active')
-	document.body.classList.add('dark')
-}
-
-// if (
-// 	window.matchMedia &&
-// 	window.matchMedia('(prefers-color-scheme: dark)').matches
-// ) {
-// 	btnDarkMode.classList.add('dark-mode-btn--active')
-// 	document.body.classList.add('dark')
-// 	localStorage.setItem('darkMode', 'dark')
-// }
-
-// window
-// 	.matchMedia('(prefers-color-scheme: dark)')
-// 	.addEventListener('change', event => {
-// 		const newColorSchema = event.matches ? 'dark' : 'light'
-
-// 		if (newColorSchema === 'dark') {
-// 			btnDarkMode.classList.add('dark-mode-btn--active')
-// 			document.body.classList.add('dark')
-// 			localStorage.setItem('darkMode', 'dark')
-// 		} else {
-// 			btnDarkMode.classList.remove('dark-mode-btn--active')
-// 			document.body.classList.remove('dark')
-// 			localStorage.setItem('darkMode', 'light')
-// 		}
-// 	})
-btnDarkMode.onclick = function (e) {
-	btnDarkMode.classList.toggle('dark-mode-btn--active')
-	document.body.classList.toggle('dark')
-
-	const isDark = document.body.classList.contains('dark')
-
+// Функция для обновления состояния темной темы
+function updateDarkMode(isDark) {
 	if (isDark) {
+		btnDarkMode.classList.add('dark-mode-btn--active')
+		document.body.classList.add('dark')
 		localStorage.setItem('darkMode', 'dark')
 	} else {
+		btnDarkMode.classList.remove('dark-mode-btn--active')
+		document.body.classList.remove('dark')
 		localStorage.setItem('darkMode', 'light')
 	}
+}
+
+if (localStorage.getItem('darkMode') === 'dark') {
+	updateDarkMode(true)
+}
+
+// Обработчик события для переключения темной темы
+btnDarkMode.onclick = function (e) {
 	e.preventDefault()
+	const isDark = document.body.classList.toggle('dark')
+	btnDarkMode.classList.toggle('dark-mode-btn--active')
+	localStorage.setItem('darkMode', isDark ? 'dark' : 'light')
+}
+
+if (window.matchMedia) {
+	const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+
+	// Устанавливаем начальное состояние в зависимости от предпочтений пользователя
+	if (mediaQuery.matches) {
+		updateDarkMode(true)
+	}
+
+	mediaQuery.addEventListener('change', event => {
+		updateDarkMode(event.matches)
+	})
 }
